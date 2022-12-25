47b165baa176319a35b5ee620e1f9cc2268c4368
haris <harisishfaq07@gmail.com>
2022-12-25 15:43:50 +0500

    add devise and root path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+
+    def after_sign_in_path_for(resource)
+        chatroom_index_path(current_user) # your path
+      end
 end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+    # root page of Application
+    def root
+    end
+end
